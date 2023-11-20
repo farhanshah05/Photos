196803398325dca1766e588b193a07703ab7e764
Farhan Shah <farhanshahirshah@gmail.com>
2023-11-20 18:45:55 -0500

    Adjustments made for better efficiency and comments added for dev. readability

--- Photos02/src/main/java/controller/AdminController.java
+++ Photos02/src/main/java/controller/AdminController.java
@@ -1,7 +1,6 @@
 package controller;
 
 import java.io.IOException;
-
 import java.util.*;
 
 import application.Photos;
@@ -12,156 +11,165 @@ import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.control.Alert.AlertType;
 import model.Superuser;
-
+import java.util.Objects;
 
 /**
- * @author Farhan Shah
- * The purpose of this class is to control the actions on the Admin Page
+ * Controller class for handling actions on the Admin Page.
  */
 public class AdminController implements LogoutController {
 
+    @FXML
+    private ListView<String> listview;
 
     @FXML
-    public ListView<String> listview;
-
-
-    @FXML
-    public Button mCreate, mDelete, mLogOff;
-
+    private Button createButton, deleteButton, logOffButton;
 
     @FXML
-    public TextField tfUsername;
+    private TextField tfUsername;
+
+    // Static list to store usernames
+    private static final ArrayList<String> userList = new ArrayList<>();
+
+    // Observable list to display usernames in the ListView
+    private ObservableList<String> observableList;
+
+    // Static instance of the Superuser class
+    private static final Superuser adminUser = Photos.photoLibraryUser;
 
     /**
-     * An ArrayList of strings that stores the added users
-     */
-    public static ArrayList<String> userlist = new ArrayList<>();
-
-    /**
-     * An observable list that helps display the list of users
-     */
-    public ObservableList<String> observableList;
-
-    /**
-     * An instance of the admin that is created to help keep track of current values
-     */
-    public static Superuser adminuser = Photos.photoLibraryUser;
-
-    /**
-     * On scene start, the listview is updated with the list of current users
+     * Initializes the Admin Page.
      */
     public void start() {
         update();
-        if(!userlist.isEmpty()) {
+        // Select the first user if the list is not empty
+        if (!userList.isEmpty()) {
             listview.getSelectionModel().select(0);
         }
     }
 
     /**
-     * Logs the user out
-     * @param event
-     * @throws IOException
+     * Logs the user out.
+     * @param event Action event triggering the method.
+     * @throws IOException If an I/O error occurs.
      */
     public void logOut(ActionEvent event) throws IOException {
         logMeOut(event);
     }
 
     /**
-     * Adds a user to the user list
-     * @param event
-     * @throws IOException
+     * Adds a new user to the system.
+     * @param event Action event triggering the method.
+     * @throws IOException If an I/O error occurs.
      */
     public void addUser(ActionEvent event) throws IOException {
-        String username = tfUsername.getText().trim();
-        if(username.isEmpty() || username == null) {
-            Alert alert = new Alert(AlertType.ERROR);
-            alert.setTitle("Admin Error");
-            alert.setContentText("Empty Field: Please enter a username.");
-            alert.showAndWait();
+        // Get and trim the entered username
+        String username = Objects.requireNonNull(tfUsername.getText()).trim();
+
+        // Check for an empty username
+        if (username.isEmpty()) {
+            showErrorAlert("Admin Error", "Empty Field", "Please enter a username.");
             return;
-        } else if(adminuser.exists(username)) {
-            Alert alert = new Alert(AlertType.ERROR);
-            alert.setTitle("Admin Error.");
-            alert.setContentText("Username already exists. Try entering a new username!");
-            alert.showAndWait();
-            return;
-        } else if (username.equals("admin")) {
-            Alert alert = new Alert(AlertType.ERROR);
-            alert.setTitle("Admin Error");
-            alert.setContentText("Cannot add 'admin' to Users.");
-            alert.showAndWait();
-            return;
-        } else {
-            adminuser.addUser(username);
-            update();
-            tfUsername.clear();
         }
-        Superuser.save(adminuser);
+
+        // Check if the username already exists
+        if (adminUser.exists(username)) {
+            showErrorAlert("Admin Error", "Username Exists", "Username already exists. Try entering a new username!");
+            return;
+        }
+
+        // Check if the username is "admin"
+        if ("admin".equals(username)) {
+            showErrorAlert("Admin Error", "Invalid Username", "Cannot add 'admin' to Users.");
+            return;
+        }
+
+        // Add the user, update the view, and save the changes
+        adminUser.addUser(username);
+        update();
+        tfUsername.clear();
+        Superuser.save(adminUser);
     }
 
     /**
-     * Removes a user from the user list
-     * @param event
-     * @throws IOException
+     * Deletes a user from the system.
+     * @param event Action event triggering the method.
+     * @throws IOException If an I/O error occurs.
      */
     public void deleteUser(ActionEvent event) throws IOException {
-
         int index = listview.getSelectionModel().getSelectedIndex();
-        int adminList = userlist.indexOf("Admin");
 
+        // Check if a user is selected
+        if (index == -1) {
+            return; // No user selected
+        }
 
+        // Confirm user deletion with a dialog
+        if (confirmDeleteUser()) {
+            // Delete the user, update the view, and save the changes
+            adminUser.deleteUser(index);
+            update();
+            Superuser.save(adminUser);
+
+            // Adjust the selection in the ListView
+            adjustListViewSelection(index);
+        }
+    }
+
+    /**
+     * Displays a confirmation dialog for user deletion.
+     * @return True if the user confirms deletion, false otherwise.
+     */
+    private boolean confirmDeleteUser() {
         Alert alert = new Alert(AlertType.CONFIRMATION);
         alert.setTitle("Confirm Delete");
         alert.setHeaderText(null);
         alert.setContentText("Are you sure you want to delete this User?");
 
         Optional<ButtonType> result = alert.showAndWait();
-        if (result.get() == ButtonType.OK) {
-
-            adminuser.deleteUser(index);
-            update();
-            Superuser.save(adminuser);
-
-            if(adminuser.getUsers().size() == 1) {
-                mDelete.setVisible(false);
-            }
-            else {
-                int lastuserindex = adminuser.getUsers().size();
-                if(adminuser.getUsers().size() == 1) {
-                    listview.getSelectionModel().select(0);
-                }
-                else if(index == lastuserindex) {
-                    listview.getSelectionModel().select(lastuserindex-1);
-                }
-                else {
-                    listview.getSelectionModel().select(index);
-                }
-            }
-
-        } else {
-            return;
-        }
-        return;
-
-
+        return result.isPresent() && result.get() == ButtonType.OK;
     }
 
     /**
-     * Refreshes the listview everytime a user is added or removed
+     * Adjusts the selection in the ListView after a user is deleted.
+     * @param index Index of the deleted user.
      */
-    public void update() {
-        userlist.clear();
-        for (int i = 0; i < adminuser.getUsers().size(); i++) {
-            userlist.add(adminuser.getUsers().get(i).getUsername());
+    private void adjustListViewSelection(int index) {
+        if (adminUser.getUsers().size() == 1) {
+            listview.getSelectionModel().select(0);
+        } else if (index == adminUser.getUsers().size()) {
+            listview.getSelectionModel().select(index - 1);
+        } else {
+            listview.getSelectionModel().select(index);
         }
-        listview.refresh();
-        observableList = FXCollections.observableArrayList(userlist);
-        listview.setItems(observableList);
-        listview.refresh();
-
-
     }
 
+    /**
+     * Updates the ListView with the latest user information.
+     */
+    public void update() {
+        userList.clear();
+        // Populate the list with usernames from the Superuser instance
+        for (int i = 0; i < adminUser.getUsers().size(); i++) {
+            userList.add(adminUser.getUsers().get(i).getUsername());
+        }
 
+        // Refresh the ListView with the updated user list
+        observableList = FXCollections.observableArrayList(userList);
+        listview.setItems(observableList);
+        listview.refresh();
+    }
 
+    /**
+     * Displays an error alert with the specified title, header, and content.
+     * @param title Alert title.
+     * @param header Alert header.
+     * @param content Alert content.
+     */
+    private void showErrorAlert(String title, String header, String content) {
+        Alert alert = new Alert(AlertType.ERROR);
+        alert.setTitle(title);
+        alert.setHeaderText(header);
+        alert.setContentText(content);
+        alert.showAndWait();
+    }
 }
