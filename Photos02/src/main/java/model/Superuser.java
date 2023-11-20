8325c9fa7f4d088940ce8de244bc108a3a0367a5
Farhan Shah <farhanshahirshah@gmail.com>
2023-11-19 19:12:58 -0500

    Added javadoc comments and better debugging logic to Superuser.java

--- Photos02/src/main/java/model/Superuser.java
+++ Photos02/src/main/java/model/Superuser.java
@@ -1,76 +1,51 @@
 package model;
 
-import application.Photos;
-
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.io.Serializable;
-import java.util.*;
+import java.io.*;
+import java.util.ArrayList;
 
 /**
- * The admin class that controls functions for adding users
- * @author Farhan Shah
- *
+ * The Superuser class manages user-related functions and user persistence.
+ * Author: Farhan Shah
  */
 public class Superuser implements Serializable {
 
-	/**
-	 * Class that manages the photo album/
-	 * Also takes care of user persistence
-	 */
 	private static final long serialVersionUID = 1L;
 	public static final String storeDir = "data";
 	public static final String storeFile = "users.dat";
 
-	/**
-	 * List of users
-	 */
-	public ArrayList<User> users;
+	private ArrayList<User> users;
+	private User current;
+	private boolean loggedIn;
 
 	/**
-	 * Current user
-	 */
-	public User current;
-
-	/**
-	 * Is the user logged in
-	 */
-	public boolean loggedIn;
-
-	/**
-	 * Constructor
+	 * Constructor initializes the Superuser object with an "admin" user.
 	 */
 	public Superuser() {
-		users = new ArrayList<User>();
+		users = new ArrayList<>();
 		users.add(new User("admin"));
 		this.current = null;
 		this.loggedIn = false;
 	}
 
 	/**
-	 * Adds a user to a list
-	 * @param username
+	 * Adds a new user to the list of users.
+	 * @param username The username of the new user.
 	 */
 	public void addUser(String username) {
 		users.add(new User(username));
 	}
 
 	/**
-	 * Deletes a user
-	 * @param index int
+	 * Deletes a user based on the given index.
+	 * @param index The index of the user to be deleted.
 	 */
 	public void deleteUser(int index) {
 		users.remove(index);
-		System.out.println(users);
 	}
 
 	/**
-	 * deletes a user
-	 * @param username username
+	 * Deletes a user based on the given username.
+	 * @param username The username of the user to be deleted.
 	 */
 	public void deleteUser(String username) {
 		User temp = new User(username);
@@ -78,50 +53,49 @@ public class Superuser implements Serializable {
 	}
 
 	/**
-	 * check if a user exists
-	 * @param username
-	 * @return
+	 * Checks if a user with the given username exists.
+	 * @param username The username to check.
+	 * @return True if the user exists, false otherwise.
 	 */
 	public boolean exists(String username) {
-		for(User user : users) {
-			if(user.getUsername().equals(username)) {
+		for (User user : users) {
+			if (user.getUsername().equals(username)) {
 				return true;
 			}
 		}
 		return false;
 	}
 
-
 	/**
-	 * check if user exists
-	 * @param user  username
-	 * @return true, exists
+	 * Checks if the provided username and password match a user in the list.
+	 * If a match is found, sets the current user and logs in.
+	 * @param user The username to check.
+	 * @return True if login is successful, false otherwise.
 	 */
 	public boolean checkUser(String user) {
 		int index = 0;
-		for(int i = 0; i < users.size(); i++) {
-			if(users.get(i).getUsername().equals(user)) {
+		for (int i = 0; i < users.size(); i++) {
+			if (users.get(i).getUsername().equals(user)) {
 				index = i;
 			}
 		}
 
-		if(index == -1) {
+		if (index == -1) {
 			return false;
 		}
 		this.setCurrent(users.get(index));
 		this.loggedIn = true;
 		return true;
-
 	}
 
 	/**
-	 * Get users current index
-	 * @return index umber of user
+	 * Gets the index of the current user in the list.
+	 * @return The index of the current user.
 	 */
 	public int getUserIndex() {
 		int index = 0;
-		for(User user : users) {
-			if(user.getUsername().equals(Photos.photoLibraryUser.getCurrent().getUsername())) {
+		for (User user : users) {
+			if (user.getUsername().equals(current.getUsername())) {
 				return index;
 			}
 			index++;
@@ -130,75 +104,87 @@ public class Superuser implements Serializable {
 	}
 
 	/**
-	 *
-	 * @return list of users
+	 * Gets the list of users.
+	 * @return The list of users.
 	 */
-	public ArrayList<User> getUsers(){
+	public ArrayList<User> getUsers() {
 		return users;
 	}
 
 	/**
-	 * Gets a user by username
-	 * @param username username
-	 * @return User instance
+	 * Gets a user based on the provided username.
+	 * @param username The username of the user to retrieve.
+	 * @return The User object corresponding to the username.
 	 */
 	public User getUser(String username) {
-		for(User user : users) {
-			if(user.getUsername().equals(username)) {
+		for (User user : users) {
+			if (user.getUsername().equals(username)) {
 				return user;
 			}
 		}
-
 		return null;
 	}
 
 	/**
-	 * Set user list
-	 * @param users
+	 * Sets the list of users.
+	 * @param users The new list of users.
 	 */
 	public void setUsers(ArrayList<User> users) {
 		this.users = users;
 	}
 
 	/**
-	 * get current user
-	 * @return current user
+	 * Gets the current user.
+	 * @return The current user.
 	 */
 	public User getCurrent() {
 		return current;
 	}
 
 	/**
-	 * Sets current user
-	 * @param current
+	 * Sets the current user.
+	 * @param current The new current user.
 	 */
 	public void setCurrent(User current) {
 		this.current = current;
 	}
 
 	/**
-	 * Save's state to .dat file
-	 * @param pdApp
-	 * @throws IOException
+	 * Checks if a user is currently logged in.
+	 * @return True if a user is logged in, false otherwise.
 	 */
-	public static void save(Superuser pdApp) throws IOException {
+	public boolean isLoggedIn() {
+		return loggedIn;
+	}
+
+	/**
+	 * Logs out the current user.
+	 */
+	public void logout() {
+		this.loggedIn = false;
+	}
+
+	/**
+	 * Saves the state of the Superuser object to a .dat file.
+	 * @param superuserInstance The Superuser instance to save.
+	 * @throws IOException If an I/O error occurs.
+	 */
+	public static void save(Superuser superuserInstance) throws IOException {
 		ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(storeDir + File.separator + storeFile));
-		oos.writeObject(pdApp);
+		oos.writeObject(superuserInstance);
 		oos.close();
 	}
 
 	/**
-	 * Loads from dat file
-	 * @return userlist
-	 * @throws IOException
-	 * @throws ClassNotFoundException
+	 * Loads a Superuser object from a .dat file.
+	 * @return The loaded Superuser object.
+	 * @throws IOException            If an I/O error occurs.
+	 * @throws ClassNotFoundException If the class of the serialized object cannot be found.
 	 */
 	public static Superuser load() throws IOException, ClassNotFoundException {
 		ObjectInputStream ois = new ObjectInputStream(new FileInputStream(storeDir + File.separator + storeFile));
 		Superuser userList = (Superuser) ois.readObject();
 		ois.close();
 		return userList;
-
 	}
-
 }
